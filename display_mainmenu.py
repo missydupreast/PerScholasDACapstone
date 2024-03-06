fceddfb8646ac6f64001ca833233333e83a35d0e
missydupreast <98125097+missydupreast@users.noreply.github.com>
2024-03-06 09:33:33 -0600

    Clarified wording in main menu

--- display_mainmenu.py
+++ display_mainmenu.py
@@ -2,8 +2,8 @@ def display_mainmenu():
     # Function to display the menu options
     print("Please select an option:")
     print("1 - View transactions by zip code and month/year")
-    print("2 - View transactions by type")
-    print("3 - View transactions by branch's state")
+    print("2 - View transaction totals by type")
+    print("3 - View transaction totals by branch's state")
     print("4 - View customer account details")
     print("5 - Modify customer account details")
     print("6 - Generate monthly credit card bill")
